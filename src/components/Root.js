8a9ef49b791a5b7604afbcb8fed3499ece0c818b
alisabeata <alisa314159@gmail.com>
2019-05-23 15:59:18 +0300

    settings

---
+++ src/components/Root.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Root extends Component {
+  state = {};
+
+  render() {
+    return <div>Root Component</div>;
+  }
+}
+
+export default Root;
---
+++ src/redux/reducer.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux';
+import { routerReduser as router } from 'react-router-redux';
+
+export default combineReducers({ router });
