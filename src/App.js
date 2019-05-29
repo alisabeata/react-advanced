fc64376ff29ab6f19a38358156fea36184f865d7
alisabeata <alisa314159@gmail.com>
2019-05-29 12:22:47 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
       <ConnectedRouter history={history}>
         <Root />
       </ConnectedRouter>
-      <Root />
     </Provider>
   );
 }
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { AdminPage } from './routes/AdminPage';
+import { AuthPage } from './routes/AuthPage';
 
 class Root extends Component {
   state = {};
 
   render() {
-    return <div>Root Component</div>;
+    return (
+      <div>
+        <Route path="/admin" component={AdminPage} />
+        <Route path="/auth" component={AuthPage} />
+      </div>
+    );
   }
 }
 
